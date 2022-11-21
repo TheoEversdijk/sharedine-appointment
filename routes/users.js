b380db2fc224f4a05faa3d87861240646e6a8e6d
Jochem Dijkdrent <dijk0146@hz.nl>
2022-11-21 13:57:08 +0100

    work in progress

--- routes/users.js
+++ routes/users.js
@@ -1,32 +1,13 @@
-import { MongoClient } from "mongodb";
-import bodyParser from 'body-parser';
-import express from 'express'
-import dotenv from 'dotenv'
-dotenv.config()
-const jsonParser = bodyParser.json()
-const EXAMPLEROUTER = express.Router()
+import express from 'express';
+import { getAppointments, setAppointments } from '../controllers/appointmentController.js';
+const router = express.Router();
 
-const DBUser = process.env.DB_USER || "";
-const DBPassword = process.env.DB_PASSWORD || "";
-const url = `mongodb+srv://${DBUser}:${DBPassword}@tjourney.sbi37ec.mongodb.net/?retryWrites=true&w=majority`;
+// routes
+router.get('/', (req, res, next) => {
+  res.json('hi');
+});
 
-EXAMPLEROUTER.get("/", async (req, res, next) => {
-    const client = await MongoClient.connect(url, { useNewUrlParser: true })
-        .catch(err => { console.log(err); });
+router.get('/appointments', getAppointments);
+router.post('/appointments', setAppointments);
 
-    if (!client) {
-        return;
-    }
-
-    try {
-        const data = await client.db("<DB>").collection("<COLLECTION>").find()
-        const dataArray = await data.toArray()
-        client.close();
-
-        res.status(200).send(dataArray)
-    } catch (err) {
-        res.status(500).send(err);
-    }
-})
-
-export default EXAMPLEROUTER
+export default router;
