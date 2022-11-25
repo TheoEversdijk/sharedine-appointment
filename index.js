dc6ef8a00edec75006d026e5119cf8b73f152867
xTheNightShade (Kevin Wang) <wang0071@hz.nl>
2022-11-25 13:43:58 +0100

    FIXED CORS

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 import express from 'express';
 import * as dotenv from 'dotenv';
 dotenv.config({ path: 'variables.env' });
+import cors from "cors";
 import appointmentrouter from './routes/appointments.js';
 
 const app = express();
@@ -10,9 +11,11 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+app.use(cors("Access-Control-Allow-Origin: *"))
+
 app.use('/', appointmentrouter);
 
-app.set('port', process.env.PORT || 3000);
+app.set('port', process.env.PORT || 3002);
 const server = app.listen(app.get('port'), () => {
   console.log(`🍿 Express running → PORT ${server.address().port}`);
 });
